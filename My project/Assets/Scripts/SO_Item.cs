f519522393d5c35cb912988dbd9f1d8321f02b5d
GuillaumeCharvet <guicharvet@orange.fr>
2023-05-13 12:06:51 +0200

    Setup directory + so_items

---
+++ My project/Assets/Scripts/SO_Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/Item", order = 1)]
+public class SO_Item : ScriptableObject
+{
+    public string itemName;
+    public GameObject prefab;
+    public int itemTier;
+    public AudioClip itemSound;
+    public bool authorized;
+}
